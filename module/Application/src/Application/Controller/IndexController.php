16a0e7edf7d4cc4c1f030d35a5eb7a22eec9773e
junior oaks <junaooaks@gmail.com>
2015-03-23 12:08:18 -0300

    nao recebe formulario

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,25 +1,49 @@
 <?php
+
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
-        $cliente = array('id'=>'1','nome'=>'JOSE');
-        
-        return new ViewModel(array('dados'=>$cliente));
+class IndexController extends AbstractActionController {
+
+    private $em;
+
+    protected function getEm() {
+        if (null === $this->em)
+            $this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+
+        return $this->em;
     }
-    
+
+    public function indexAction() {
+        $cliente = array('id' => '1', 'nome' => 'JOSE');
+
+        return new ViewModel(array('dados' => $cliente));
+    }
+
     public function newAction() {
-        
-        
-        
-        //retirecionar para a pagina onde esta o formulario
-        //return $this->redirect()->toRoute('application', array('controller' => 'Application\Controller\Index','action'=>'new'));
-        
+
+        $form = $this->params()->fromPost();
+
+        if (!empty($form)) {
+            
+            print_r($form);
+            die;
+            
+            //preencher os dados do formulario
+            $form->setData($form);
+
+            $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+
+            //pegar o repositorio da entidade
+            $repo = $em->getRepository('Cliente\Entity\Cliente');
+
+            //buscar todos os dados da tabela cliente
+            $dados = $repo->findAll();
+        }
+
         return new ViewModel();
     }
+
 }
